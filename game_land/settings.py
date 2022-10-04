be7d044ae792ef13970542e7fb1562a735d5ef0d
Hugo <hcmr@cesar.school>
2022-10-04 18:16:35 -0300

    pronto para deploy

--- game_land/settings.py
+++ game_land/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = 'django-insecure-eueckk)mm8@)@k(tz0af#yt#mv6*z=40@_)-9s-2de9zgglgb#
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['https://gameeland.herokuapp.com/']
 
 
 # Application definition
@@ -127,3 +128,5 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+django_heroku.settings(locals())
